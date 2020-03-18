b82feaf240cf0b7c256e5094e2cdddfc2a5c8c6b
Wojciech Dudek <wdudek82@gmail.com>
2020-03-18 21:48:53 +0100

    refactor null check

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const linkedStylesheets = document.querySelectorAll('link[rel="stylesheet"]');
 const timestamp = new Date().getTime();
 
 function removeQueryParam(url) {
-  if (!url) {
-    return;
+  if (url) {
+    return url.split('?')[0];
   }
-  return url.split('?')[0];
 }
 
 function main() {
   for (const linkedStylesheet of linkedStylesheets) {
     const url = removeQueryParam(linkedStylesheet.getAttribute('href'));
+    
     if (url) {
       linkedStylesheet.setAttribute('href', `${url}?time=${timestamp}`)
     }
